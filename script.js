82a0ccd20e8fad2d5d16b29c7421fc16ba422edd
seth <seth.barthen@gmail.com>
2020-10-12 17:30:08 -0500

    updates to code

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const clicklyBtn = document.querySelector('#changeColor');
 
 
-
-
 function changeBackgroundColor() {
-    
-}
+    const colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080'];
+    const random = Math.floor(Math.random() * colors.length);
 
+    document.body.style.backgroundColor = colors[random];
+
+    console.log(random);
+}
 
 clicklyBtn.addEventListener('click', changeBackgroundColor);
